6fb7dc7a79794a578796c3c224462ac119ca988c
brunosilvafreitass <brunodasilvafreitas@outlook.com>
2025-04-08 00:16:04 -0300

    Add .gitignore

--- Ticket.Api/obj/Debug/net9.0/Ticket.Api.AssemblyInfo.cs
+++ Ticket.Api/obj/Debug/net9.0/Ticket.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ticket.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f6a7fdb018910427546ef6ecb2ca73738926eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ticket.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ticket.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
